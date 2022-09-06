80bfce684b8e34103b0617571fd19ede81575580
Iaroslav87 <91834197+Iaroslav87@users.noreply.github.com>
2022-09-06 13:25:32 -0700

    lesson_7 Email via celery

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+if DEBUG:
+    INTERNAL_IPS = ["192.168.0.179" "127.0.0.1"]
+
 
 # Application definition
 
@@ -42,6 +45,7 @@ INSTALLED_APPS = [
     "mainapp",
     "authapp",
     "crispy_forms",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -52,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
